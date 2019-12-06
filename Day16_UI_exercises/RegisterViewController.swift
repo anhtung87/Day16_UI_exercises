2417dd22109b9bafecf57fdfae71f6fa230c23b1
anhtung87 <tungha.acb@gmail.com>
2019-12-06 10:07:29 +0700

    login + register screen

---
+++ Day16_UI_exercises/RegisterViewController.swift
@@ -0,0 +1,21 @@
+//
+//  RegisterViewController.swift
+//  Day16_UI_exercises
+//
+//  Created by Hoang Tung on 12/6/19.
+//  Copyright © 2019 Hoang Tung. All rights reserved.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func onLogin(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+}
---
+++ Day16_UI_exercises/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Day16_UI_exercises
+//
+//  Created by Hoang Tung on 12/5/19.
+//  Copyright © 2019 Hoang Tung. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
+
